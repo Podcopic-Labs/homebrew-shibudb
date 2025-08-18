ad6b7c70c2d5e1b05c047688f57205734c25adee
Prabhat Awasthi <90645975+prabhat3697@users.noreply.github.com>
2025-08-18 18:21:45 +0530

    Update shibudb.rb

--- Formula/shibudb.rb
+++ Formula/shibudb.rb
@@ -1,7 +1,7 @@
 class Shibudb < Formula
   desc "Lightweight embedded database with KV and vector support"
   homepage "https://github.com/shibudb-org/shibudb-server"
-  url "https://github.com/user-attachments/files/21777762/shibudb-1.0.1-darwin-arm64.tar.gz"
+  url "https://github.com/user-attachments/files/21835565/shibudb-1.0.1-darwin-arm64.tar.gz"
   sha256 "109b9b57a0c3290d3814919aa03b55fadb284cc002d7c86a2f7b3d9d68385260"
   version "1.0.1"
   license "AGPL3"
